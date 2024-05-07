7cf270dd78760e46a5006bd281afd31c1838081d
alsiam09 <156769180+alsiam09@users.noreply.github.com>
2024-05-07 22:34:41 +0600

    first commit

--- src/component/ProdectSlick.jsx
+++ src/component/ProdectSlick.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
 import Slider from 'react-slick';
 import prodectimg from '../assets/prodectimg.png'
+import { IoIosArrowBack } from "react-icons/io";
+import { GrFormNext } from "react-icons/gr";
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div className="h-[40px] text-[20px] text-[#fff] absolute top-[50%] left-[10px] transform-y-[-50%] w-[40px] flex justify-center items-center rounded-[50%] bg-[#c4c4c4]"
+      onClick={onClick}>
+        <IoIosArrowBack/>
+      </div>
+    );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div 
+      className="h-[40px] text-[20px] text-[#fff] absolute top-[50%] z-[3] right-[10px] transform-y-[-50%] w-[40px] flex justify-center items-center rounded-[50%] bg-[#c4c4c4]"
+      onClick={onClick}
+      ><GrFormNext/></div>
+    );
+  }
 const ProdectSlick = () => {
     const settings = {
         dots: false,
@@ -8,6 +29,8 @@ const ProdectSlick = () => {
         speed: 500,
         slidesToShow: 4,
         slidesToScroll: 4,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />,
         responsive: [
             {
                 breakpoint: 768,
@@ -16,6 +39,7 @@ const ProdectSlick = () => {
                   dots: false,
                   slidesToShow: 2 ,
                   slidesToScroll: 2,
+                  arrows:false,
                 }
               },
             
@@ -25,29 +49,33 @@ const ProdectSlick = () => {
     <div className="container mx-auto">
  <div className="slider-container">
       <Slider {...settings}>
-        <div className=" h-[220px] sm:h-[240px] lg:h-[320px] bg-[#fff] xl:h-[390px] 2xl:h-[470px] p-[5px] bg-[#fff] Prodect_box">
-            <img src={prodectimg} className='h-[80%] '  alt="" />
+        <div className=" relative p-[5px] bg-[#fff] Prodect_box">
+            <span className=' absolute top-[20px] left-[24px] py-[9px] px-[32px] bg-[#262626] text-[#fff] text-[16px] font-[700]'>New</span>
+            <img src={prodectimg} className=' '  alt="" />
             <div className="name flex justify-between">
                 <h2 className="text-[13px] md:text-[20px] w-[70%]" >Basic Crew  Neck Tee</h2>
                 <span className="font-[400] text-[#767676] text-[13px] md:text-[20px]">$44.00</span>
             </div>
         </div>
-        <div className=" h-[220px] sm:h-[240px] lg:h-[320px] bg-[#fff] xl:h-[390px] 2xl:h-[470px] p-[5px] bg-[#fff] Prodect_box">
-            <img src={prodectimg} className='h-[80%] '  alt="" />
+        <div className=" relative p-[5px] bg-[#fff] Prodect_box">
+            <span className=' absolute top-[20px] left-[24px] py-[9px] px-[32px] bg-[#262626] text-[#fff] text-[16px] font-[700]'>New</span>
+            <img src={prodectimg} className=' '  alt="" />
             <div className="name flex justify-between">
                 <h2 className="text-[13px] md:text-[20px] w-[70%]" >Basic Crew  Neck Tee</h2>
                 <span className="font-[400] text-[#767676] text-[13px] md:text-[20px]">$44.00</span>
             </div>
         </div>
-        <div className=" h-[220px] sm:h-[240px] lg:h-[320px] bg-[#fff] xl:h-[390px] 2xl:h-[470px] p-[5px] bg-[#fff] Prodect_box">
-            <img src={prodectimg} className='h-[80%] '  alt="" />
+        <div className=" relative p-[5px] bg-[#fff] Prodect_box">
+            <span className=' absolute top-[20px] left-[24px] py-[9px] px-[32px] bg-[#262626] text-[#fff] text-[16px] font-[700]'>New</span>
+            <img src={prodectimg} className=' '  alt="" />
             <div className="name flex justify-between">
                 <h2 className="text-[13px] md:text-[20px] w-[70%]" >Basic Crew  Neck Tee</h2>
                 <span className="font-[400] text-[#767676] text-[13px] md:text-[20px]">$44.00</span>
             </div>
         </div>
-        <div className=" h-[220px] sm:h-[240px] lg:h-[320px] bg-[#fff] xl:h-[390px] 2xl:h-[470px] p-[5px] bg-[#fff] Prodect_box">
-            <img src={prodectimg} className='h-[80%] '  alt="" />
+        <div className=" relative p-[5px] bg-[#fff] Prodect_box">
+            <span className=' absolute top-[20px] left-[24px] py-[9px] px-[32px] bg-[#262626] text-[#fff] text-[16px] font-[700]'>New</span>
+            <img src={prodectimg} className=' '  alt="" />
             <div className="name flex justify-between">
                 <h2 className="text-[13px] md:text-[20px] w-[70%]" >Basic Crew  Neck Tee</h2>
                 <span className="font-[400] text-[#767676] text-[13px] md:text-[20px]">$44.00</span>
