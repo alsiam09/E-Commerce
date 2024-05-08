315137004bc3a482f3a1e581c9d01a9045087af2
alsiam09 <156769180+alsiam09@users.noreply.github.com>
2024-05-08 06:23:49 +0600

    first commit

--- src/component/ProdectSlick.jsx
+++ src/component/ProdectSlick.jsx
@@ -4,6 +4,8 @@ import prodectimg from '../assets/prodectimg.png'
 import { IoIosArrowBack } from "react-icons/io";
 import { GrFormNext } from "react-icons/gr";
 import { GiEternalLove } from "react-icons/gi";
+import { GoGitCompare } from "react-icons/go";
+import { FaShoppingCart } from "react-icons/fa";
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
   return (
@@ -24,11 +26,6 @@ function SamplePrevArrow(props) {
   );
 }
 const ProdectSlick = () => {
-  let [show, setshow] = useState(false);
-  function onclick() {
-    setshow(!show)
-    console.log("ami");
-  }
   const settings = {
     dots: false,
     infinite: true,
@@ -53,17 +50,71 @@ const ProdectSlick = () => {
   };
   return (
     <div className="container mx-auto">
+      <h2 className=' text-[30px] ml-1 font-sans lg:text-[39px] font-[700] pb-[20px] lg:pb-[48px]'>New Arrivals</h2>
       <div className="slider-container">
         <Slider {...settings}>
-          <div onClick={onclick} className=" p-[5px] bg-[#fff] Prodect_box">
+          <div className=" p-[5px] bg-[#fff] Prodect_box">
             <div  className="img relative">
               <span className='text-[10px] py-[5px] px-[12px] absolute top-[20px] left-[24px] lg:py-[9px] lg:px-[32px] bg-[#262626] text-[#fff] lg:text-[16px] lg:font-[700]'>New</span>
               <img  src={prodectimg} className=' ' alt="" />
-              {show == true && <ul className="p-[5px] lg:p-[10px] pro_in_box bg-[#fff] h-[45%] absolute bottom-0 left-0 w-[100%]">
+              <div className="hov  opacity-0 hover:opacity-[1] p-[5px] lg:p-[10px] pro_in_box h-[100%] absolute bottom-0 left-0 w-[100%]">
+              <ul className=" p-[5px] lg:p-[10px] pro_in_box bg-[#fff] h-[45%] absolute bottom-0 left-0 w-[100%]">
                 <li className=' items-center flex justify-end ' > <span className='pr-[5px] text-[12px] lg:text-[16px] font-[700] text-[#767676] hover:text-[#000] py-[3px] font-sans'>Add to Wish List</span> <div className="icon inline-block"><GiEternalLove /></div> </li>
+                <li className=' items-center flex justify-end ' > <span className='pr-[5px] text-[12px] lg:text-[16px] font-[700] text-[#767676] hover:text-[#000] py-[3px] font-sans'>Compare</span> <div className="icon inline-block"><GoGitCompare /></div> </li>
+                <li className=' items-center flex justify-end ' > <span className='pr-[5px] text-[12px] lg:text-[16px] font-[700] text-[#767676] hover:text-[#000] py-[3px] font-sans'>Add to Cart</span> <div className="icon inline-block"><FaShoppingCart/></div> </li>
+              </ul>
+              </div>
+            </div>
+            <div className="name flex justify-between">
+              <h2 className="text-[13px] lg:text-[20px] w-[70%]" >Basic Crew  Neck Tee <span className='block text-[12px] lg:text-[16px] font-sans text-[#767676]' >Back</span></h2>
+              <span className="font-[400] text-[#767676] text-[13px] lg:text-[20px]">$44.00</span>
+            </div>
+          </div>
+          <div className=" p-[5px] bg-[#fff] Prodect_box">
+            <div  className="img relative">
+              <span className='text-[10px] py-[5px] px-[12px] absolute top-[20px] left-[24px] lg:py-[9px] lg:px-[32px] bg-[#262626] text-[#fff] lg:text-[16px] lg:font-[700]'>New</span>
+              <img  src={prodectimg} className=' ' alt="" />
+              <div className="hov  opacity-0 hover:opacity-[1] p-[5px] lg:p-[10px] pro_in_box h-[100%] absolute bottom-0 left-0 w-[100%]">
+              <ul className=" p-[5px] lg:p-[10px] pro_in_box bg-[#fff] h-[45%] absolute bottom-0 left-0 w-[100%]">
                 <li className=' items-center flex justify-end ' > <span className='pr-[5px] text-[12px] lg:text-[16px] font-[700] text-[#767676] hover:text-[#000] py-[3px] font-sans'>Add to Wish List</span> <div className="icon inline-block"><GiEternalLove /></div> </li>
+                <li className=' items-center flex justify-end ' > <span className='pr-[5px] text-[12px] lg:text-[16px] font-[700] text-[#767676] hover:text-[#000] py-[3px] font-sans'>Compare</span> <div className="icon inline-block"><GoGitCompare /></div> </li>
+                <li className=' items-center flex justify-end ' > <span className='pr-[5px] text-[12px] lg:text-[16px] font-[700] text-[#767676] hover:text-[#000] py-[3px] font-sans'>Add to Cart</span> <div className="icon inline-block"><FaShoppingCart/></div> </li>
+              </ul>
+              </div>
+            </div>
+            <div className="name flex justify-between">
+              <h2 className="text-[13px] lg:text-[20px] w-[70%]" >Basic Crew  Neck Tee <span className='block text-[12px] lg:text-[16px] font-sans text-[#767676]' >Back</span></h2>
+              <span className="font-[400] text-[#767676] text-[13px] lg:text-[20px]">$44.00</span>
+            </div>
+          </div>
+          <div className=" p-[5px] bg-[#fff] Prodect_box">
+            <div  className="img relative">
+              <span className='text-[10px] py-[5px] px-[12px] absolute top-[20px] left-[24px] lg:py-[9px] lg:px-[32px] bg-[#262626] text-[#fff] lg:text-[16px] lg:font-[700]'>New</span>
+              <img  src={prodectimg} className=' ' alt="" />
+              <div className="hov  opacity-0 hover:opacity-[1] p-[5px] lg:p-[10px] pro_in_box h-[100%] absolute bottom-0 left-0 w-[100%]">
+              <ul className=" p-[5px] lg:p-[10px] pro_in_box bg-[#fff] h-[45%] absolute bottom-0 left-0 w-[100%]">
                 <li className=' items-center flex justify-end ' > <span className='pr-[5px] text-[12px] lg:text-[16px] font-[700] text-[#767676] hover:text-[#000] py-[3px] font-sans'>Add to Wish List</span> <div className="icon inline-block"><GiEternalLove /></div> </li>
-              </ul>}
+                <li className=' items-center flex justify-end ' > <span className='pr-[5px] text-[12px] lg:text-[16px] font-[700] text-[#767676] hover:text-[#000] py-[3px] font-sans'>Compare</span> <div className="icon inline-block"><GoGitCompare /></div> </li>
+                <li className=' items-center flex justify-end ' > <span className='pr-[5px] text-[12px] lg:text-[16px] font-[700] text-[#767676] hover:text-[#000] py-[3px] font-sans'>Add to Cart</span> <div className="icon inline-block"><FaShoppingCart/></div> </li>
+              </ul>
+              </div>
+            </div>
+            <div className="name flex justify-between">
+              <h2 className="text-[13px] lg:text-[20px] w-[70%]" >Basic Crew  Neck Tee <span className='block text-[12px] lg:text-[16px] font-sans text-[#767676]' >Back</span></h2>
+              <span className="font-[400] text-[#767676] text-[13px] lg:text-[20px]">$44.00</span>
+            </div>
+          </div>
+          <div className=" p-[5px] bg-[#fff] Prodect_box">
+            <div  className="img relative">
+              <span className='text-[10px] py-[5px] px-[12px] absolute top-[20px] left-[24px] lg:py-[9px] lg:px-[32px] bg-[#262626] text-[#fff] lg:text-[16px] lg:font-[700]'>New</span>
+              <img  src={prodectimg} className=' ' alt="" />
+              <div className="hov  opacity-0 hover:opacity-[1] p-[5px] lg:p-[10px] pro_in_box h-[100%] absolute bottom-0 left-0 w-[100%]">
+              <ul className=" p-[5px] lg:p-[10px] pro_in_box bg-[#fff] h-[45%] absolute bottom-0 left-0 w-[100%]">
+                <li className=' items-center flex justify-end ' > <span className='pr-[5px] text-[12px] lg:text-[16px] font-[700] text-[#767676] hover:text-[#000] py-[3px] font-sans'>Add to Wish List</span> <div className="icon inline-block"><GiEternalLove /></div> </li>
+                <li className=' items-center flex justify-end ' > <span className='pr-[5px] text-[12px] lg:text-[16px] font-[700] text-[#767676] hover:text-[#000] py-[3px] font-sans'>Compare</span> <div className="icon inline-block"><GoGitCompare /></div> </li>
+                <li className=' items-center flex justify-end ' > <span className='pr-[5px] text-[12px] lg:text-[16px] font-[700] text-[#767676] hover:text-[#000] py-[3px] font-sans'>Add to Cart</span> <div className="icon inline-block"><FaShoppingCart/></div> </li>
+              </ul>
+              </div>
             </div>
             <div className="name flex justify-between">
               <h2 className="text-[13px] lg:text-[20px] w-[70%]" >Basic Crew  Neck Tee <span className='block text-[12px] lg:text-[16px] font-sans text-[#767676]' >Back</span></h2>
